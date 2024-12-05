023d1a0abf781f0ca9821afae0f8f5e1f2a742b7
winniehhy <yoyowinnie3@gmail.com>
2024-12-05 08:28:26 +0800

    norminette ok

--- src/monitor.c
+++ src/monitor.c
@@ -30,18 +30,20 @@ static void	set_end_status(t_table *table, bool status)
 */
 static bool	kill_philo(t_philo *philo)
 {
-    time_t	current_time;
-    time_t  buffer = 1; // Small buffer to prevent race conditions
+	time_t	current_time;
+	time_t	buffer;
 
-    current_time = get_time_in_ms();
-    if ((current_time - philo->last_meal) >= (philo->table->time_to_die - buffer))
-    {
-        set_end_status(philo->table, true);
-        print_status(philo, "has died", true, "\033[0;31m");
-        pthread_mutex_unlock(&philo->meal_time_lock);
-        return (true);
-    }
-    return (false);
+	buffer = 1;
+	current_time = get_time_in_ms();
+	if ((current_time - philo->last_meal)
+		>= (philo->table->time_to_die - buffer))
+	{
+		set_end_status(philo->table, true);
+		print_status(philo, "has died", true, "\033[0;31m");
+		pthread_mutex_unlock(&philo->meal_time_lock);
+		return (true);
+	}
+	return (false);
 }
 
 /*
